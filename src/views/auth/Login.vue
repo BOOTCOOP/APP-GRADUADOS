489b46fb7195c0c984cafe762daf30014fc10e19
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2025-02-11 19:44:17 -0300

    Refactor social media icons and links

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,79 +1,116 @@
 <template>
-    <graduados-blank body="white" :hideFabButton="true">        
-        <Form ref="form" class="content">
-            <ion-img src="/assets/logo/logo.png" />
-            <div>
-                <Field v-model="email" name="email" v-slot="{ field }" rules="required">
-                    <IonItem>
-                        <IonLabel position="floating">Nombre</IonLabel>
-                        <IonInput v-bind="field"/>
-                    </IonItem>
-                    <ErrorMessage name="email" #default="{message}"> <ion-text color="danger"><small>{{message}}</small></ion-text></ErrorMessage>
-                </Field>
-                
-                <Field name="password" v-model="password" label="contraseña" v-slot="{ field }" rules="required">
-                    <FormPassword v-model="password" v-bind="field" label="Contraseña"/>
-                    <ErrorMessage name="password" #default="{message}"> <ion-text color="danger"><small>{{message}}</small></ion-text></ErrorMessage>
-                </Field>
-                <div>
+  <graduados-blank body="white" :hideFabButton="true">
+    <Form ref="form" class="content">
+      <ion-img src="/assets/logo/logo.png" />
+      <div>
+        <Field v-model="email" name="email" v-slot="{ field }" rules="required">
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput v-bind="field" />
+          </IonItem>
+          <ErrorMessage name="email" #default="{ message }">
+            <ion-text color="danger"
+              ><small>{{ message }}</small></ion-text
+            ></ErrorMessage
+          >
+        </Field>
 
-                    <ion-text color="medium" router-link="/recuperar-contrasena" class="ion-margin-vertical"><small>¿Olvidaste tu contraseña?</small></ion-text>
-                </div>
-            </div>
-            <div>
-                <ion-button @click="login()" :disabled="sending" expand="full" shape="round">{{ sending ? 'Ingresando...' : 'Ingresar' }}</ion-button>                
-                <div class="ion-text-center">
-                    <ion-text color="medium">¿Aún no tenés una cuenta? <span @click="register()">Registrate</span> </ion-text>
-                </div>
-            </div>
-        </Form>
-    </graduados-blank>
+        <Field
+          name="password"
+          v-model="password"
+          label="contraseña"
+          v-slot="{ field }"
+          rules="required"
+        >
+          <FormPassword v-model="password" v-bind="field" label="Contraseña" />
+          <ErrorMessage name="password" #default="{ message }">
+            <ion-text color="danger"
+              ><small>{{ message }}</small></ion-text
+            ></ErrorMessage
+          >
+        </Field>
+        <div>
+          <ion-text
+            color="medium"
+            router-link="/recuperar-contrasena"
+            class="ion-margin-vertical"
+            ><small>¿Olvidaste tu contraseña?</small></ion-text
+          >
+        </div>
+      </div>
+      <div>
+        <ion-button
+          @click="login()"
+          :disabled="sending"
+          expand="full"
+          shape="round"
+          >{{ sending ? 'Ingresando...' : 'Ingresar' }}</ion-button
+        >
+        <div class="ion-text-center">
+          <ion-text color="medium"
+            >¿Aún no tenés una cuenta?
+            <span @click="register()">Registrate</span>
+          </ion-text>
+        </div>
+      </div>
+    </Form>
+  </graduados-blank>
 </template>
 
 <script setup lang="ts">
-    import { IonButton, IonInput, IonItem, IonLabel, IonImg, IonText } from '@ionic/vue';
-    import FormPassword from "@/views/app/components/form/FormPassword.vue"; 
-    import { useIonRouter } from '@ionic/vue';
-    import { Form, Field, ErrorMessage } from "vee-validate";
-    import { ref } from 'vue';
-    import { useAuth } from "@/uses/auth";
+import { useAuth } from '@/uses/auth'
+import FormPassword from '@/views/app/components/form/FormPassword.vue'
+import {
+  IonButton,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonText,
+  useIonRouter,
+} from '@ionic/vue'
+import { ErrorMessage, Field, Form } from 'vee-validate'
+import { ref } from 'vue'
 
-    const sending = ref(false);
-    const form = ref(false);
-    const email = ref("user@mail.com")
-    const password = ref("123456")
+const sending = ref(false)
+const form = ref(false)
+const email = ref('user@mail.com')
+const password = ref('123456')
 
-    const ionRouter = useIonRouter();
+const ionRouter = useIonRouter()
 
-    function login(){
-        form.value.validate().then( v => {
-            if(v.valid){
-                sending.value = true;
-                useAuth().login(email.value, password.value).then(() => {
-                    ionRouter.navigate('/', 'forward', 'replace');
-                }).catch(err => {
-                    form.value.setErrors(err.response.data || {email: "Hubo un error"});
-                    sending.value = false;
-                });
-            }
+function login() {
+  form.value.validate().then((v) => {
+    if (v.valid) {
+      sending.value = true
+      useAuth()
+        .login(email.value, password.value)
+        .then(() => {
+          ionRouter.navigate('/', 'forward', 'replace')
+        })
+        .catch((err) => {
+          form.value.setErrors(err.response.data || { email: 'Hubo un error' })
+          sending.value = false
         })
     }
+  })
+}
 
-    function register(){
-        ionRouter.push({name: 'register'});
-    }
+function register() {
+  ionRouter.push({ name: 'register' })
+}
 </script>
 
 <style scoped>
-    .content{
-        min-height: calc(100vh - var(--ion-padding, 16px) - var(--ion-padding, 16px));
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around
-    }
+.content {
+  min-height: calc(100vh - var(--ion-padding, 16px) - var(--ion-padding, 16px));
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
 
-    ion-img{
-        width: 80%;
-        margin: 0 auto;
-    }
+ion-img {
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
--- src/views/home/Social.vue
+++ src/views/home/Social.vue
@@ -1,62 +1,84 @@
 <template>
-    <div class="ion-margin-top socials">
-        <ion-grid>
-            <ion-row>
-                <ion-col>
-                    <ion-card @click="goToFacebook()" class="ion-no-margin ion-margin-top ion-text-center social">
-                        <ion-card-content>
-                            <ion-icon :md="logoFacebook" size="large" :ios="logoFacebook"></ion-icon>
-                        </ion-card-content>
-                    </ion-card>
-                </ion-col>
-                <ion-col>
-                    <ion-card @click="goToTwitter()" class="ion-no-margin ion-margin-top ion-text-center social">
-                        <ion-card-content>
-                            <ion-icon :md="logoTwitter" size="large" :ios="logoTwitter"></ion-icon>
-                        </ion-card-content>
-                    </ion-card>
-                </ion-col>
-                <ion-col>
-                    <ion-card @click="goToInstagram()" class="ion-no-margin ion-margin-top ion-text-center social">
-                        <ion-card-content>
-                            <ion-icon :md="logoInstagram" size="large" :ios="logoInstagram"></ion-icon>
-                        </ion-card-content>
-                    </ion-card>
-                </ion-col>
-            </ion-row>
-        </ion-grid>
-    </div>
+  <div class="ion-margin-top socials">
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ion-card
+            @click="goToFacebook()"
+            class="ion-no-margin ion-margin-top ion-text-center social"
+          >
+            <ion-card-content>
+              <ion-icon
+                :md="logoFacebook"
+                size="large"
+                :ios="logoFacebook"
+              ></ion-icon>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card
+            @click="goToTwitter()"
+            class="ion-no-margin ion-margin-top ion-text-center social"
+          >
+            <ion-card-content>
+              <font-awesome-icon
+                icon="fa-brands fa-x-twitter"
+                font-size="36px"
+              />
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+        <ion-col>
+          <ion-card
+            @click="goToInstagram()"
+            class="ion-no-margin ion-margin-top ion-text-center social"
+          >
+            <ion-card-content>
+              <ion-icon
+                :md="logoInstagram"
+                size="large"
+                :ios="logoInstagram"
+              ></ion-icon>
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+  </div>
 </template>
 
 <script setup lang="ts">
-    import {
-        logoTwitter,
-        logoFacebook,
-        logoInstagram
-    } from 'ionicons/icons';
+import { logoFacebook, logoInstagram } from 'ionicons/icons'
 
-    import {
-        IonCard, IonCardContent,
-        IonGrid, IonCol, IonRow,
-        IonIcon
-    } from '@ionic/vue';
+import {
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonGrid,
+  IonIcon,
+  IonRow,
+} from '@ionic/vue'
 
-    function goToFacebook(){
-        window.open('https://www.facebook.com/centrode.graduados', '_system');
-    }
-    
-    function goToTwitter(){
-        window.open('https://twitter.com/GraduadoDchoUBA', '_system');
-    }
-    
-    function goToInstagram(){
-        window.open('https://www.instagram.com/centrodegraduados/', '_system');
-    }
-    
+function goToFacebook() {
+  window.open('https://www.facebook.com/centrode.graduados', '_system')
+}
+
+function goToTwitter() {
+  window.open('https://x.com/GraduadoDchoUBA', '_system')
+}
+
+function goToInstagram() {
+  window.open('https://www.instagram.com/centrodegraduados/', '_system')
+}
 </script>
 
 <style scoped>
-    ion-card.social{
-        background-color: var(--ion-color-step-900);
-    }
+ion-card.social {
+  background-color: var(--ion-color-step-900);
+}
+
+.share-button font-awesome-icon {
+  font-size: 42px;
+}
 </style>
