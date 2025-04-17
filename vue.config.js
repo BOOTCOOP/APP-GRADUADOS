4c2b62dc320b26b33b86cf7ffdbe9e331663344e
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2025-04-17 13:01:56 -0300

    Update API URL and add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,5 @@
+// vue.config.js
+module.exports = {
+  // Cuando hagas build en producción, que use como base /APP‑GRADUADOS/
+  publicPath: process.env.NODE_ENV === 'production' ? '/APP-GRADUADOS/' : '/',
+}
