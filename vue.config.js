04a51356689590eebeae44178300a26d835a8025
Miguel Maidana <maidana.miguell@gmail.com>
2025-04-17 22:02:02 -0300

    version completa con descarga de archivos parcial , solo descarga uno

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-// vue.config.js
+// // vue.config.js
+// module.exports = {
+//   // Cuando hagas build en producción, que use como base /APP‑GRADUADOS/
+//   publicPath: process.env.NODE_ENV === 'production' ? '/APP-GRADUADOS/' : '/',
+// }
 module.exports = {
-  // Cuando hagas build en producción, que use como base /APP‑GRADUADOS/
-  publicPath: process.env.NODE_ENV === 'production' ? '/APP-GRADUADOS/' : '/',
-}
+  publicPath: "./",
+};
