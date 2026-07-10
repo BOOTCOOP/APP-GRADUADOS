4c2a2a7a57de7b491e162caba0005bc519c5c9ef
LautaroRiveiro <LautaroRiveiro@users.noreply.github.com>
2026-07-10 22:03:36 +0000

    deploy: 5c6bfbcf4c61d47304591d3dc7a442dc65bc6652

---
+++ js/8336-legacy.0ba1216e.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkgraduados_app"]=self["webpackChunkgraduados_app"]||[]).push([[8336],{8336(e,t,s){var n=s(6061),a=s(4534),r=s(9185);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const o=()=>{const e=window;e.addEventListener("statusTap",()=>{(0,n.gv)(()=>{const t=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(t/2,s/2);if(!o)return;const d=(0,a.a)(o);d&&new Promise(e=>(0,r.c)(d,e)).then(()=>{(0,n.bN)(async()=>{d.style.setProperty("--overflow","hidden"),await(0,a.s)(d,300),d.style.removeProperty("--overflow")})})})})};s.d(t,["startStatusTap",0,o])}}]);
+//# sourceMappingURL=8336-legacy.0ba1216e.js.map
